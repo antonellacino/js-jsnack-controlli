d1ca08db46b725a564580ad8808584250fbe1408
Antonella Cino <antonella.cino18@gmail.com>
2020-03-20 16:43:04 +0100

    esercizio1 stilizzato

--- esercizio1/js/main.js
+++ esercizio1/js/main.js
@@ -1,14 +1,17 @@
 //1 - L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
 
 //creo le due variabili contenenti il numero inserito dall'utente
-var msg;
+var msg, msgErrore;
 var numero1 = parseInt(prompt('Inserisci un numero'));
 var numero2 = parseInt(prompt('Inserisci un secondo numero'));
 //indetifico tra i due numeri qual'è il maggiore
-if (numero1>numero2) {
-  msg = "Il numero più grande è: " + numero1;
+if (numero1 === numero2) {
+  alert("Hai inserito lo stesso numero..Riprova!");
+} else if (numero1>numero2){
+  msg = numero1;
 } else {
-  msg = "Il numero più grande è: " + numero2;
+  msg = numero2;
 }
+
 //rimando a video la risposta
 document.getElementById('risposta').innerHTML= msg;
