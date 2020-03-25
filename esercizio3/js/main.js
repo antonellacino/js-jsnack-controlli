385e84774ca8cd135f96110dbaec9151ba1a48a0
Antonella Cino <antonella.cino18@gmail.com>
2020-03-25 13:06:06 +0100

    versione con il for

--- esercizio3/js/main.js
+++ esercizio3/js/main.js
@@ -2,7 +2,7 @@
 //Il programma stampa la somma di tutti i numeri inseriti.
 
 //chiedo all'utente di inserire i 5 numeri e li salvo in una variabile
-var num1 = parseInt(prompt('Inserisci un numero'));
+/*var num1 = parseInt(prompt('Inserisci un numero'));
 var num2 = parseInt(prompt('Inserisci il secondo numero'));
 var num3 = parseInt(prompt('Inserisci il terzo numero'));
 var num4 = parseInt(prompt('Inserisci il quarto'));
@@ -11,4 +11,16 @@ var num5 = parseInt(prompt('Infine...il quintoooo!'));
 var somma = num1 + num2 +num3 + num4 + num5;
 console.log(somma);
 //mostro a video il risultato
-document.getElementById('somma').innerHTML = "La somme dei numeri da te inserita è: " + somma;
+document.getElementById('somma').innerHTML = "La somme dei numeri da te inserita è: " + somma;*/
+
+
+/*Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
+*/
+var somma = 0;
+for (var i = 0; i < 5; i++) {
+
+  var num = parseInt(prompt('Inserisci un numero'));
+  console.log(num);
+  var somma = somma+num;
+  console.log(somma);
+}
